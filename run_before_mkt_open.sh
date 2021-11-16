f0991bb9e5265926db17e804325593211398f2ad
matthew <iwasnothing@gmail.com>
2021-11-16 23:05:13 +0800

    new signals files

--- run_before_mkt_open.sh
+++ run_before_mkt_open.sh
@@ -4,5 +4,7 @@ cd /Users/kahingleung/PycharmProjects/timeseries/venv/bin
 source activate
 cd ../..
 python rsi.py
-rm -Rf .darts/checkpoints
+rm -Rf .darts/checkpoints .darts/untrained_models
+chown kahingleung:staff *.sh *.py *.html *.csv *.txt
+chown -R kahingleung:staff .git
 su - kahingleung -c "/Users/kahingleung/PycharmProjects/timeseries/upload.sh"
